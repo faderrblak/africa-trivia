475e990121362095ba4e8668ea6a901c8b4e47a3
johmfaderr <johmfaderr@gmail.com>
2020-06-17 23:17:19 +0300

    Updates

---
+++ precache-manifest.fb19b0c725bb7bd28b7b9cf518ca2d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d5c15456780781a674e437c66cc888",
+    "url": "/africa-trivia/index.html"
+  },
+  {
+    "revision": "b2a4a0adc3ea06e1156c",
+    "url": "/africa-trivia/static/css/main.7e5171d6.chunk.css"
+  },
+  {
+    "revision": "eb48ac634074a90d191c",
+    "url": "/africa-trivia/static/js/2.9ace9f51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/africa-trivia/static/js/2.9ace9f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a4a0adc3ea06e1156c",
+    "url": "/africa-trivia/static/js/main.c5309cc4.chunk.js"
+  },
+  {
+    "revision": "3d7c5df8b00077c0caff",
+    "url": "/africa-trivia/static/js/runtime-main.e3082493.js"
+  }
+]);
