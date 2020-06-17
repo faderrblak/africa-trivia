14a6367f2e10b4731bac08d6cc56ec93d93a3170
johmfaderr <johmfaderr@gmail.com>
2020-06-17 23:08:38 +0300

    create Africa Trivia page with multiple components

---
+++ src/components/Result.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Result = ({score, playAgain}) => (
+    <div className="score-board">
+        <div className="score">You scored {score} / 5 correct answers!</div>
+        <br/>
+        <button className="playBtn" onClick={playAgain}>
+            Play Again!
+        </button>
+    </div>
+);
+
+export default Result;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './style/index.css';
+import quizService from './quizService';
+import QuestionBox from './components/QuestionBox';
+import Result from './components/Result';
+
+const Quiz = () => {
+  const [questions, setQuestions] = useState([]);
+  const [score, setScore] = useState(0);
+  const [response, setResponse] = useState(0);
+
+  const getQuestions = () => {
+    quizService().then(question => {
+      setQuestions(question);
+    });
+  };
+
+  useEffect(() => {
+    getQuestions();
+  }, []);
+
+  const computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      setScore(score + 1)
+    };
+    setResponse(response < 5 ? response + 1 : 5);
+  }
+  
+  const playAgain = () => {
+    getQuestions();
+    setScore(0);
+    setResponse(0);
+  };
+
+  return(
+    <div className="page">
+      <div className="title">Africa Trivia</div>
+      {response < 5 ? (<div className="instruction">Select an answer. Once you've answered all 5 you will see your result in the next page.</div>) : null}
+      {questions.length > 0 && 
+        response < 5 && 
+        questions.map(({question, answers, correct, questionId}) => (
+        <QuestionBox 
+          question={question} 
+          options={answers} 
+          key={questionId}
+          selected={answer => computeAnswer(answer, correct)}
+        />
+      ))}
+      {response === 5 ? (<Result score={score} playAgain={playAgain}/>) : null}
+      <div className="footer">{String.fromCharCode(169) + ' 2020. Faderr Johm'}</div>
+    </div>
+  )
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Quiz/>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
